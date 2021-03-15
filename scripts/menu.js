ea56c8e6f5bf19594121ce8f2c518dd0f46f49c2
Evan Fang <evanfang.hi@gmail.com>
2021-03-15 11:29:49 +0800

    Done

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,18 @@
-$(function(){
-	$(document).on('click', '.menuBtn.close', function() {
-	    $(this).addClass("open");
-	    $(this).removeClass("close");
-	    $(".menuList").css("margin-top", "0");
-	});
-	$(document).on('click', '.menuBtn.open', function() {
-	    $(this).addClass("close");
-	    $(this).removeClass("open");
-	    $(".menuList").css("margin-top", "-100%");
-	});
-	$('a').on('click',function(){
-    	if($(this).attr('scroll-to')){
-    		gScroll($($(this).attr('scroll-to')).offset().top-$('header').height());
-    		return false;
-    	}
-    })
+$(function () {
+  $(document).on("click", ".menuBtn.close", function () { 
+    $(this).addClass("open");
+    $(this).removeClass("close");
+    $(".menuList").removeClass("menuOpen").addClass("menuClose");
+  });
+  $(document).on("click", ".menuBtn.open", function () {
+    $(this).addClass("close");
+    $(this).removeClass("open");
+    $(".menuList").removeClass("menuClose").addClass("menuOpen");
+  });
+  $("a").on("click", function () {
+    if ($(this).attr("scroll-to")) {
+      gScroll($($(this).attr("scroll-to")).offset().top - $("header").height());
+      return false;
+    }
+  });
 });
